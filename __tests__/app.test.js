5e6c617429557ed4a092377ec5adf7ac7573a2c2
Eduardo <eduardor.telecom@gmail.com>
2024-04-30 14:51:49 +0000

    new commit

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,7 +4,4 @@ describe('Testing arithmetic operations', () => {
   test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
   });
-  test('subtracts 5 - 2 to equal 3', () => {
-    expect(subtract(5, 2)).toBe(3);
-  });
 });
